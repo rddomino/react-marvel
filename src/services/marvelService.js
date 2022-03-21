c110191ef79a916376dfc38240a6f2bbeab336d6
rddomino <seltos@bk.ru>
2022-03-21 17:42:59 +0600

    Реализация компонента вывода рандомного героя

--- src/services/marvelService.js
+++ src/services/marvelService.js
@@ -1,5 +1,7 @@
-
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=a553a4f91870033a0869df8d58c35a63';
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -10,8 +12,30 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=a553a4f91870033a0869df8d58c35a63');
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        if (!char.description) {
+            char.description = 'Sorry, no description...';
+        } else if (char.description.length > 200) {
+            char.description = char.description.substring(0, 200) + '...';
+        }
+
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
